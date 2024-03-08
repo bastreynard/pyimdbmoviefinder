6be247e0497cc7a4c3706f9f7961ad2e2b7ddace
Bastien Reynard <bastien@rnrd.ch>
2024-03-08 23:10:06 +0100

    Fix pip dependencies

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='pyimdbmoviefinder', version='1.0', packages=find_packages(), entry_points={
-        'console_scripts': ['pyimdbmoviefinder=pyimdbmoviefinder.cli.clisearch:cli'] # so this directly refers to a function available in __init__.py
-        })
+setup(name='pyimdbmoviefinder', version='1.0', packages=find_packages(), 
+      install_requires=['requests', 'imdbpy', 'httplib2', 'cinemagoer', 'humanize', 'configparser'],
+      entry_points={'console_scripts': ['pyimdbmoviefinder=pyimdbmoviefinder.cli.clisearch:cli']}
+      )
