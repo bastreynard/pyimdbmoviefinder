049fe8362ce9c07e92225509fb0a84c5a8c0d343
Bastien Reynard <bast.reynard@gmail.com>
2024-03-09 02:16:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
 setup(name='pyimdbmoviefinder', version='1.0', packages=find_packages(), 
-      install_requires=['requests', 'imdbpy', 'httplib2', 'cinemagoer', 'humanize', 'configparser'],
+      install_requires=['requests', 'httplib2', 'cinemagoer', 'humanize', 'configparser'],
       entry_points={'console_scripts': ['pyimdbmoviefinder=pyimdbmoviefinder.cli.clisearch:cli']}
       )
