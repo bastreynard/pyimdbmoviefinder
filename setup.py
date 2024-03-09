d195ed0d6a66e1581536ba4cfc7bbfff60a3d25c
Bastien Reynard <bastien@rnrd.ch>
2024-03-09 19:16:31 +0100

    Rework CLI, bump version

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 
 setup(name='pyimdbmoviefinder', version='1.0', packages=find_packages(), 
       install_requires=['requests', 'httplib2', 'cinemagoer', 'humanize', 'configparser'],
-      entry_points={'console_scripts': ['pyimdbmoviefinder=pyimdbmoviefinder.cli.clisearch:cli']}
+      entry_points={'console_scripts': ['pyimdbmoviefinder=cli.clisearch:cli']}
       )
